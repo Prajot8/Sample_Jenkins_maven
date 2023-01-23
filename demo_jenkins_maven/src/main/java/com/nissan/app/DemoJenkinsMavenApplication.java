255fe5d7afb8df7d5265cccce3cbd5a9d967ef74
SUNILRAO MULEY <NDH00886@Nmcorp.Nissan.Biz>
2023-01-23 23:37:04 +0530

    Project demo Added

---
+++ demo_jenkins_maven/src/main/java/com/nissan/app/DemoJenkinsMavenApplication.java
@@ -0,0 +1,14 @@
+package com.nissan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsMavenApplication.class, args);
+		System.out.println("App started sucessfully.......");
+	}
+
+}
